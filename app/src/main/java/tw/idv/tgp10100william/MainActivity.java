de324336ad55aed63f443a51cbdec4049d3a6807
William Lee <ithan0117@gmail.com>
2022-07-23 16:27:10 +0800

    add test button

--- app/src/main/java/tw/idv/tgp10100william/MainActivity.java
+++ app/src/main/java/tw/idv/tgp10100william/MainActivity.java
@@ -3,12 +3,27 @@ package tw.idv.tgp10100william;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+	private Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViews();
+		handleButton();
+	}
+
+	private void findViews() {
+		button = findViewById(R.id.button);
+	}
+
+	private void handleButton() {
+		button.setOnClickListener(view -> {
+			Toast.makeText(this, "dfkjghfdlkghfd", Toast.LENGTH_SHORT).show();
+		});
 	}
 }
